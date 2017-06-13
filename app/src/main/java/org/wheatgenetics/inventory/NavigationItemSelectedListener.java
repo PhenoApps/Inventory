bbbb243af03d7a06e77e47fe7762f7c3f4d2feb8
Peter Rosario <PeterRosario@ksu.edu>
2017-06-13 11:49:43 -0500

    Configured navigation/drawer menu.

--- app/src/main/java/org/wheatgenetics/inventory/NavigationItemSelectedListener.java
+++ app/src/main/java/org/wheatgenetics/inventory/NavigationItemSelectedListener.java
@@ -25,17 +25,17 @@ implements android.support.design.widget.NavigationView.OnNavigationItemSelected
     }
 
     @java.lang.Override
-    public boolean onNavigationItemSelected(final android.view.MenuItem item)
+    public boolean onNavigationItemSelected(
+    @android.support.annotation.NonNull final android.view.MenuItem item)
     {
         // Handle navigation view item clicks here.
         switch (item.getItemId())
         {
-            case org.wheatgenetics.inventory.R.id.nav_camera   : break;                // From
-            case org.wheatgenetics.inventory.R.id.nav_gallery  : break;                //  menu/-
-            case org.wheatgenetics.inventory.R.id.nav_slideshow: break;                //  activity-
-            case org.wheatgenetics.inventory.R.id.nav_manage   : break;                //  _main-
-            case org.wheatgenetics.inventory.R.id.nav_share    : break;                //  _drawer-
-            case org.wheatgenetics.inventory.R.id.nav_send     : break;                //  .xml.
+            case org.wheatgenetics.inventory.R.id.nav_set_person    : break;          // From menu/-
+            case org.wheatgenetics.inventory.R.id.nav_connect_scale : break;          //  activity-
+            case org.wheatgenetics.inventory.R.id.nav_export        : break;          //  _main-
+            case org.wheatgenetics.inventory.R.id.nav_delete        : break;          //  _drawer
+            case org.wheatgenetics.inventory.R.id.nav_show_about    : break;          //  .xml.
         }
 
         this.drawerCloser.closeDrawer();
