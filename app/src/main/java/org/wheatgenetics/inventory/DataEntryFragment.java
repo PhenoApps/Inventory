edbba552d9ad5b42c852887159b1bd070412278c
Peter Rosario <PeterRosario@ksu.edu>
2017-09-01 17:29:54 -0500

    DataEntryFragment: Refactored.

--- app/src/main/java/org/wheatgenetics/inventory/DataEntryFragment.java
+++ app/src/main/java/org/wheatgenetics/inventory/DataEntryFragment.java
@@ -1,121 +1,118 @@
 package org.wheatgenetics.inventory;
 
-import android.content.Context;
-import android.net.Uri;
-import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-
-
 /**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link DataEntryFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link DataEntryFragment#newInstance} factory method to
- * create an instance of this fragment.
+ * A simple {@link android.support.v4.app.Fragment} subclass.  Activities that contain this fragment
+ * must implement the
+ * {@link org.wheatgenetics.inventory.DataEntryFragment.OnFragmentInteractionListener} interface to
+ * handle interaction events.  Use the
+ * {@link org.wheatgenetics.inventory.DataEntryFragment#newInstance} factory method to create an in
+ * instance of this fragment.
+ *
+ * Uses:
+ * android.content.Context
+ * android.net.Uri
+ * android.os.Bundle
+ * android.support.v4.app.Fragment
+ * android.view.LayoutInflater
+ * android.view.View
+ * android.view.ViewGroup
+ *
+ * org.wheatgenetics.inventory.R
  */
-public class DataEntryFragment extends Fragment
+public class DataEntryFragment extends android.support.v4.app.Fragment
 {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    /**
+     * This interface must be implemented by activities that contain this fragment to allow an in-
+     * teraction in this fragment to be communicated to the activity and potentially other fragments
+     * contained in that activity.
+     * <p>See the Android Training lesson <a
+     * href="http://developer.android.com/training/basics/fragments/communicating.html">Communicating
+     * with Other Fragments</a> for more information.</p>
+     */
+    interface OnFragmentInteractionListener
+    { /* TODO: Update argument type and name. */ void onFragmentInteraction(android.net.Uri uri); }
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    // TODO: Rename parameter arguments: choose names that match the fragment initialization parame-
+    // TODO: ters, e.g., ARG_ITEM_NUMBER.
+    private static final java.lang.String ARG_PARAM1 = "param1", ARG_PARAM2 = "param2";
 
-    private OnFragmentInteractionListener mListener;
+    // region Fields
+    // TODO: Rename and change types of parameters.
+    private java.lang.String param1, param2;
 
-    public DataEntryFragment()
+    private org.wheatgenetics.inventory.DataEntryFragment.OnFragmentInteractionListener listener;
+    // endregion
+
+    public DataEntryFragment() { /* Required empty public constructor. */ }
+
+    // region Overridden Methods
+    @java.lang.Override
+    public void onAttach(final android.content.Context context)
     {
-        // Required empty public constructor
+        super.onAttach(context);
+
+        if (context instanceof
+        org.wheatgenetics.inventory.DataEntryFragment.OnFragmentInteractionListener)
+            this.listener =
+                (org.wheatgenetics.inventory.DataEntryFragment.OnFragmentInteractionListener)
+                    context;
+        else
+        {
+            assert null != context; throw new java.lang.RuntimeException(context.toString() +
+                " must implement OnFragmentInteractionListener");
+        }
     }
 
+    @java.lang.Override
+    public void onCreate(final android.os.Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+
+        final android.os.Bundle args = this.getArguments();
+        if (null != args)
+        {
+            this.param1 = args.getString(org.wheatgenetics.inventory.DataEntryFragment.ARG_PARAM1);
+            this.param2 = args.getString(org.wheatgenetics.inventory.DataEntryFragment.ARG_PARAM2);
+        }
+    }
+
+    @java.lang.Override
+    public android.view.View onCreateView(final android.view.LayoutInflater inflater,
+    final android.view.ViewGroup container, final android.os.Bundle savedInstanceState)
+    {
+        // Inflate the layout for this fragment:
+        assert null != inflater; return inflater.inflate(
+            org.wheatgenetics.inventory.R.layout.fragment_data_entry, container, false);
+    }
+
+    @java.lang.Override
+    public void onDetach() { super.onDetach(); this.listener = null; }
+    // endregion
+
+    // TODO: Rename method, update argument and hook method into UI event.
+    public void onButtonPressed(final android.net.Uri uri)
+    { if (listener != null) listener.onFragmentInteraction(uri); }
+
     /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
+     * Use this factory method to create a new instance of this fragment using the provided
+     * parameters.
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
      * @return A new instance of fragment DataEntryFragment.
      */
-    // TODO: Rename and change types and number of parameters
-    public static DataEntryFragment newInstance(String param1, String param2)
+    // TODO: Rename and change types and number of parameters.
+    public static org.wheatgenetics.inventory.DataEntryFragment newInstance(
+    final java.lang.String param1, final java.lang.String param2)
     {
-        DataEntryFragment fragment = new DataEntryFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null)
+        org.wheatgenetics.inventory.DataEntryFragment result =
+            new org.wheatgenetics.inventory.DataEntryFragment();
         {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+            final android.os.Bundle args = new android.os.Bundle();
+            args.putString(org.wheatgenetics.inventory.DataEntryFragment.ARG_PARAM1, param1);
+            args.putString(org.wheatgenetics.inventory.DataEntryFragment.ARG_PARAM2, param2);
+            result.setArguments(args);
         }
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-        Bundle savedInstanceState)
-    {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_data_entry, container, false);
-    }
-
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri)
-    {
-        if (mListener != null)
-        {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
-
-    @Override
-    public void onAttach(Context context)
-    {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener)
-        {
-            mListener = (OnFragmentInteractionListener) context;
-        } else
-        {
-            throw new RuntimeException(context.toString()
-                + " must implement OnFragmentInteractionListener");
-        }
-    }
-
-    @Override
-    public void onDetach()
-    {
-        super.onDetach();
-        mListener = null;
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener
-    {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
+        return result;
     }
 }
