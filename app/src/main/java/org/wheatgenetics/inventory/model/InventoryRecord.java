b1b0f3a4ac4735f8ea115e2c498c8696f89d5682
Peter Rosario <PeterRosario@ksu.edu>
2017-09-28 08:35:53 -0500

    org.wheatgenetics.inventory.model: Added package and its classes.

---
+++ app/src/main/java/org/wheatgenetics/inventory/model/InventoryRecord.java
@@ -0,0 +1,114 @@
+package org.wheatgenetics.inventory.model;
+
+/**
+ * Uses:
+ * android.util.Log
+ *
+ * org.wheatgenetics.javalib.Utils
+ */
+public class InventoryRecord extends java.lang.Object
+{
+    // region Fields
+    private int              id                                                  ;
+    private java.lang.String box = null, envid = null, person = null, date = null;
+    private int              position                                            ;
+    private java.lang.String wt = null                                           ;
+    // endregion
+
+    // region Constructors
+    // region Public Constructors
+    public InventoryRecord() { super(); }
+
+    public InventoryRecord(final java.lang.String id, final java.lang.String box,
+    final java.lang.String envid, final java.lang.String person, final java.lang.String date,
+    final java.lang.String position, final java.lang.String wt)
+    { this(); this.set(id, box, envid, person, date, position, wt); }
+    // endregion
+
+    // region Package Constructors
+    InventoryRecord(final java.lang.String box, final java.lang.String envid, final int position)
+    { this(); this.box = box; this.envid = envid; this.position = position; }
+
+    InventoryRecord(final java.lang.String box, final java.lang.String envid,
+    final java.lang.String person, final int position, final java.lang.String wt)
+    {
+        this(box, envid, position);
+
+        this.person = person                                       ;
+        this.date   = org.wheatgenetics.javalib.Utils.getDateTime();
+        this.wt     = wt                                           ;
+    }
+    // endregion
+    // endregion
+
+    @java.lang.Override
+    public java.lang.String toString()
+    {
+        return this.getBox() + "," + this.getEnvId() + "," + this.getPerson() +
+            "," + this.getDate() + "," + this.getPosition() + "," + this.getWt();
+    }
+
+    // region Package Methods
+    void setId(final int id) { this.id = id; }
+
+    java.lang.String getTag()
+    { return this.getBox() + "," + this.getEnvId() + "," + this.getPositionAsString(); }
+
+    java.lang.String getCSV()
+    {
+        return this.getBox() + ","  + this.getEnvId() + "," + this.getDate() +
+            "," + this.getPerson() + "," + this.getWt() + "\r\n";
+    }
+
+    java.lang.String getSQL()
+    {
+        final java.lang.String fields[] = this.toString().split(",");
+        for (int i = 0; i < fields.length; i++)
+            if (fields[i].length() <= 0)
+                fields[i] = "null";
+            else
+                if (!fields[i].equals("null")) fields[i] = "'" + fields[i] + "'";
+
+        return "(" +
+            fields[0] + "," +  // box
+            fields[1] + "," +  // envid
+            fields[3] + "," +  // date
+            fields[2] + "," +  // person
+            fields[5] + ")" ;  // wt
+    }
+
+    int sendErrorLogMsg(final java.lang.String tag)
+    {
+        return android.util.Log.e(tag, this.getBox() + " " +
+            this.getPositionAsString() + " " + this.getEnvId() + " " + this.getWt());
+    }
+    // endregion
+
+    // region Public Methods
+    // region Getter Public Methods
+    public int              getId      () { return this.id      ; }
+    public java.lang.String getBox     () { return this.box     ; }
+    public java.lang.String getEnvId   () { return this.envid   ; }
+    public java.lang.String getPerson  () { return this.person  ; }
+    public java.lang.String getDate    () { return this.date    ; }
+    public int              getPosition() { return this.position; }
+    public java.lang.String getWt      () { return this.wt      ; }
+    // endregion
+
+    public java.lang.String getPositionAsString()
+    { return java.lang.Integer.toString(this.getPosition()); }
+
+    public void set(final java.lang.String id, final java.lang.String box,
+    final java.lang.String envid, final java.lang.String person, final java.lang.String date,
+    final java.lang.String position, final java.lang.String wt)
+    {
+        this.setId(java.lang.Integer.parseInt(id));
+
+        this.box = box; this.envid = envid; this.person = person; this.date = date;
+        this.position = java.lang.Integer.parseInt(position); this.wt = wt;
+    }
+
+    public int sendDebugLogMsg(final java.lang.String tag)
+    { return android.util.Log.d(tag, this.toString()); }
+    // endregion
+}
---
+++ app/src/main/java/org/wheatgenetics/inventory/model/InventoryRecords.java
@@ -0,0 +1,113 @@
+package org.wheatgenetics.inventory.model;
+
+/**
+ * Uses:
+ * android.util.Log
+ *
+ * org.wheatgenetics.inventory.model.InventoryRecord
+ */
+public class InventoryRecords extends java.lang.Object
+implements java.lang.Iterable<org.wheatgenetics.inventory.model.InventoryRecord>
+{
+    private final java.util.LinkedList<org.wheatgenetics.inventory.model.InventoryRecord>
+        linkedList = new java.util.LinkedList<org.wheatgenetics.inventory.model.InventoryRecord>();
+
+    @java.lang.Override
+    public java.util.Iterator<org.wheatgenetics.inventory.model.InventoryRecord> iterator()
+    { return this.linkedList.iterator(); }
+
+    // region Public Methods
+    public boolean add(final org.wheatgenetics.inventory.model.InventoryRecord inventoryRecord)
+    { return this.linkedList.add(inventoryRecord); }
+
+    public java.io.File writeCSV(final java.io.File file)
+    {
+        if (this.linkedList.isEmpty())
+            return null;
+        else
+        {
+            {
+                java.io.FileOutputStream fileOutputStream;
+                try { fileOutputStream = new java.io.FileOutputStream(file); }   // throws java.io.-
+                catch (final java.io.FileNotFoundException e) { return null; }   //  FileNotFoundEx-
+                                                                                 //  ception
+                {
+                    final java.io.OutputStreamWriter outputStreamWriter =
+                        new java.io.OutputStreamWriter(fileOutputStream);
+                    try
+                    {
+                        outputStreamWriter.append(                     // throws java.io.IOException
+                            "box_id,seed_id,inventory_date,inventory_person,weight_gram\r\n");
+                        for (final org.wheatgenetics.inventory.model.InventoryRecord
+                        inventoryRecord: this.linkedList)
+                        {
+                            assert inventoryRecord != null;
+                            outputStreamWriter.append(inventoryRecord.getCSV()); // throws java.io.-
+                        }                                                        //  IOException
+                    }
+                    catch (final java.io.IOException e) { return null; }
+
+                    try { outputStreamWriter.close(); /* throws java.io.IOException */ }
+                    catch (final java.io.IOException e) { return file; }
+                }
+
+                try { fileOutputStream.close(); /* throws java.io.IOException */ }
+                catch (final java.io.IOException e) { return file; }
+            }
+            return file;
+        }
+    }
+
+    public java.io.File writeSQL(final java.io.File file, final java.lang.String boxList)
+    {
+        if (this.linkedList.isEmpty())
+            return null;
+        else
+        {
+            {
+                java.io.FileOutputStream fileOutputStream;
+                try { fileOutputStream = new java.io.FileOutputStream(file); }   // throws java.io.-
+                catch (final java.io.FileNotFoundException e) { return null; }   //  FileNotFoundEx-
+                                                                                 //  ception
+                {
+                    final java.io.OutputStreamWriter outputStreamWriter =
+                        new java.io.OutputStreamWriter(fileOutputStream);
+
+                    try
+                    {
+                        outputStreamWriter.append(                     // throws java.io.IOException
+                                "DELETE FROM seedinv WHERE seedinv.box_id in " + boxList + ";\n")
+                            .append(                                   // throws java.io.IOException
+                                "INSERT INTO seedinv(`box_id`,`seed_id`,`inventory_date`," +
+                                "`inventory_person`,`weight_gram`)\r\nVALUES");
+                        {
+                            java.lang.StringBuffer body = new java.lang.StringBuffer();
+                            {
+                                final int  first = 0, last = this.linkedList.size() - 1;
+                                      char terminator = ','                            ;
+                                for (int i = first; i <= last; i++)
+                                {
+                                    if (last == i) terminator = ';';
+                                    body.append(
+                                        this.linkedList.get(i).getSQL() + terminator + "\r\n");
+                                }
+                            }
+                            outputStreamWriter.append(body.toString());      // throws
+                        }                                                    //  java.io.IOException
+                    }
+                    catch (final java.io.IOException e) { return null; }
+
+                    try { outputStreamWriter.close(); /* throws java.io.IOException */ }
+                    catch (final java.io.IOException e) { return file; }
+                }
+                try { fileOutputStream.close(); /* throws java.io.IOException */ }
+                catch (final java.io.IOException e) { return file; }
+            }
+            return file;
+        }
+    }
+
+    public int sendDebugLogMsg(final java.lang.String tag)
+    { return android.util.Log.d(tag, this.toString()); }
+    // endregion
+}
